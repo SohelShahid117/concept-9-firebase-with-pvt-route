8e707ad0b950231a872248a15a92021ee849fbcd
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-28 22:15:20 +0600

    start nJoying firebase auth with pvt route

---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./Component/Root/Root";
+import Products from "./Component/Products/Products";
+import About from "./Component/About/About";
+import Contact from "./Component/Contact/Contact";
+import Blog from "./Component/Blog/Blog";
+import Login from "./Component/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Products></Products>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

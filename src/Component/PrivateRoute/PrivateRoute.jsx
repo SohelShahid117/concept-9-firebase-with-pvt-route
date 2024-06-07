c42acde483b17e8bdfd865e81813df8ea82b1bed
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-08 00:05:53 +0600

    Conceptual Sessions-9-firebase with pvt rout part-4 done

--- src/Component/PrivateRoute/PrivateRoute.jsx
+++ src/Component/PrivateRoute/PrivateRoute.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import useAuth from "../../hooks/useAuth";
+import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoute = () => {
-  return <div></div>;
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
+  // console.log(user);
+  const location = useLocation();
+  console.log(location);
+  if (!user) {
+    // return <h2>Loading...</h2>;
+    return <Navigate to="/login" state={location?.pathname || "/"}></Navigate>;
+  }
+  return (
+    <div>
+      <h1>This is private route</h1>
+      {children}
+    </div>
+  );
 };
 
 export default PrivateRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Register from "./Component/Register/Register";
 import ProductDetails from "./Component/ProductDetails/ProductDetails";
 import Hero from "./Component/Hero/Hero";
 import Book from "./Component/Book/Book";
+import PrivateRoute from "./Component/PrivateRoute/PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -32,17 +33,25 @@ const router = createBrowserRouter([
       },
       {
         path: `/book/:id`,
-        element: <Book></Book>,
+        element: (
+          <PrivateRoute>
+            <Book></Book>
+          </PrivateRoute>
+        ),
         loader: () => fetch(`/books.json`),
       },
       {
         path: "/product",
         element: <Products></Products>,
       },
-      {
-        path: "/product:id",
-        element: <ProductDetails></ProductDetails>,
-      },
+      // {
+      //   path: "/product:id",
+      //   element: (
+      //     <PrivateRoute>
+      //       <ProductDetails></ProductDetails>
+      //     </PrivateRoute>
+      //   ),
+      // },
       {
         path: "/about",
         element: <About></About>,
